d3ec6975468727b51f8460edd5a749fa8baf9962
jlloyd57 <46935070+jlloyd57@users.noreply.github.com>
2020-07-15 17:31:37 -0400

    Updated project code

---
+++ corona.py
@@ -0,0 +1,120 @@
+# Load libraries
+from pandas import read_csv
+from pandas.plotting import scatter_matrix
+from matplotlib import pyplot
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+print("I have loaded the libraries")
+# Load dataset
+url = "https://raw.githubusercontent.com/jlloyd57/PythonFinal/master/No%20Label%20Corona%20Total.csv"
+names = ['date', 'daily confirmed cases','confirmed cases total', 'deaths', 'daily death toll', 'Month']
+dataset = read_csv(url, names=names)
+print("I have loaded the dataset")
+# shape- how many rows (43) and columns (6)
+print(dataset.shape)
+#Looking at the first 20 rows of the data
+print(dataset.head(20))
+# statistical summary-
+print(dataset.describe())
+# looking at # rows that belong in each class
+print(dataset.groupby('Month').size())
+#box and whisker plots of each
+dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+pyplot.show()
+#histograms
+dataset.hist()
+pyplot.show()
+#scatter plot matrix
+scatter_matrix(dataset)
+pyplot.show()
+# Split-out validation dataset
+array = dataset.values
+X = array[:,1:5]
+y = array[:,5]
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.20, random_state=1)
+# Spot Check Algorithms
+models = []
+models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC(gamma='auto')))
+# evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+    kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+    cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
+    results.append(cv_results)
+    names.append(name)
+    print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
+# Compare Algorithms
+pyplot.boxplot(results, labels=names)
+pyplot.title('Algorithm Comparison')
+pyplot.show()
+#make predictions on validation dataset
+print("Based off the whisker plots please enter which model you want it to predict")
+prediction=input('Which model?')
+prediction
+#model = LinearDiscriminantAnalysis()
+if(prediction == "LR"):
+    model = LogisticRegression(solver='liblinear', multi_class='ovr')
+    model.fit(X_train, Y_train)
+    predictions = model.predict(X_validation)
+# Evaluate predictions
+    print(accuracy_score(Y_validation, predictions))
+    print(confusion_matrix(Y_validation, predictions))
+    print(classification_report(Y_validation, predictions))
+elif(prediction == "LDA") :
+    model = LinearDiscriminantAnalysis()
+    model.fit(X_train, Y_train)
+    predictions = model.predict(X_validation)
+# Evaluate predictions
+    print(accuracy_score(Y_validation, predictions))
+    print(confusion_matrix(Y_validation, predictions))
+    print(classification_report(Y_validation, predictions))
+elif(prediction == "KNN"):
+    model = KNeighborsClassifier()
+    model.fit(X_train, Y_train)
+    predictions = model.predict(X_validation)
+# Evaluate predictions
+    print(accuracy_score(Y_validation, predictions))
+    print(confusion_matrix(Y_validation, predictions))
+    print(classification_report(Y_validation, predictions))
+elif(prediction == "CART"):
+    model = DecisionTreeClassifier()
+    model.fit(X_train, Y_train)
+    predictions = model.predict(X_validation)
+# Evaluate predictions
+    print(accuracy_score(Y_validation, predictions))
+    print(confusion_matrix(Y_validation, predictions))
+    print(classification_report(Y_validation, predictions))
+elif(prediction == "NB"):
+    model = GaussianNB()
+    model.fit(X_train, Y_train)
+    predictions = model.predict(X_validation)
+# Evaluate predictions
+    print(accuracy_score(Y_validation, predictions))
+    print(confusion_matrix(Y_validation, predictions))
+    print(classification_report(Y_validation, predictions))
+else:
+    model = SVC(gamma='auto')
+    model.fit(X_train, Y_train)
+    predictions = model.predict(X_validation)
+# Evaluate predictions
+    print(accuracy_score(Y_validation, predictions))
+    print(confusion_matrix(Y_validation, predictions))
+    print(classification_report(Y_validation, predictions))
+print("done")
+
